764f2b9691939bc19d6697faf23ca3b801efb987
olliedirker <olliedirker@gmail.com>
2022-08-02 13:20:41 -0500

    tested the candidates return of data

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require("mysql2");
 //express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host:'localhost',
+        //your MySQL username,
+        user: 'dirker',
+        //your MySQL password
+        password:'Lorelei09072017!',
+        database:'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows)=> {
+    console.log(rows);
+})
+
+db.query(`SELECT * FROM candidates`)
   //add get route that is for requests not supported by the app
   //Default response for any other request(Not found)
   app.use((req, res)=> {
