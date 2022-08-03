208aa3ab982bf52d2d4ecc0dbfac99845a8c265c
olliedirker <olliedirker@gmail.com>
2022-08-02 19:22:13 -0500

    add CRUD routes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const { ifError } = require('assert');
 const express = require('express');
+const inputCheck = require('./utils/inputCheck');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mysql = require("mysql2");
@@ -60,11 +62,11 @@ app.delete('/api/candidate/:id', (req, res) => {
     db.query(sql, params, (err, result) => {
         if (err) {
             res.statusMessage(400).json({ error: res.message });
-        }else if(!result.affectedRows){
+        } else if (!result.affectedRows) {
             res.json({
-                message:'Candidate not found'
+                message: 'Candidate not found'
             });
-        }else{
+        } else {
             res.json({
                 message: 'succesfully deleted',
                 changes: result.affectedRows,
@@ -77,33 +79,32 @@ app.delete('/api/candidate/:id', (req, res) => {
 // // Create a candidate
 app.post('/api/candidate', ({ body }, res) => {
     const errors = inputCheck(
-      body,
-      'first_name',
-      'last_name',
-      'industry_connected'
+        body,
+        'first_name',
+        'last_name',
+        'industry_connected'
     );
     if (errors) {
-      res.status(400).json({ error: errors });
-      return;
+        res.status(400).json({ error: errors });
+        return;
     }
-  
+
     const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
       VALUES (?,?,?)`;
     const params = [body.first_name, body.last_name, body.industry_connected];
-  
-    db.query(sql, params, (err, result) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: body
-      });
-    });
-  });
 
-db.query(`SELECT * FROM candidates`)
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
 //add get route that is for requests not supported by the app
 //Default response for any other request(Not found)
 app.use((req, res) => {
